0fbab977014c8ad762f953c232f7f8d8fab42734
ParatrooperAndy <1468492471@qq.com>
2022-05-23 11:36:33 +0800

    update test002

--- test002.py
+++ test002.py
@@ -4,15 +4,6 @@ findspark.init()
 
 from pyspark.sql import SparkSession
 # from pyspark.sql.functions import col
-import os
-
-# 注意这里右键在terminal里跑代码，并且需要在头部表明coding为utf-8
-
-# 指定一下Java环境防止Exception: Java gateway process exited before sending its port number。
-# 这里注意pyspark版本需和spark对应，此代码在pycharm可以执行，但vscode出错。
-os.environ['JAVA_HOME'] = r"C://Java//jdk1.8.0_333"
-os.environ['SPARK_HOME_PATH'] = r"D://spark-2.3.2-bin-hadoop2.7"
-os.environ['HADOOP_HOME_PATH'] = r"D://winutils//hadoop-2.7.3"
 
 # 初始化
 spark = SparkSession.builder.master("local[*]").appName("FirstAPP").getOrCreate()
