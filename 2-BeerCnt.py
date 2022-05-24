78f71fdcc6165a4dcb7792e1efeff624bcafd386
ParatrooperAndy <1468492471@qq.com>
2022-05-24 20:21:03 +0800

    update 2

--- 2-BeerCnt.py
+++ 2-BeerCnt.py
@@ -31,11 +31,13 @@ def remove0sales():
     # 1）去除整月销量为 0 的数据
     # 如果过去三周平均销量为0，则月销量即为0，且数据可信不出错，将其过滤去除
     lines = lineRDD.filter(lambda x: "0" != x[3])
-    # 再次过滤去除4、5、6列之和的平均值不等于第3列的数据
-    lines2 = lines.filter(lambda x: abs(int(x[4]) + int(x[5]) + int(x[6]) - 3*int(x[3])) < 1)
+    # 再次过滤去除4、5、6列之和的平均值不等于第3列的数据，
+    # 由于小数点与整数计算误差，这里应该让误差在原有小数点计算的误差1以内，乘三计算即为3
+    lines2 = lines.filter(lambda x: abs(int(x[4]) + int(x[5]) + int(x[6]) - 3*int(x[3])) < 3)
     for item in lines2.collect():
         # 使用format格式化输出，共8列
-        print("{0},{1},{2},{3},{4},{5},{6},{7}".format(item[0], item[1], item[2], item[3], item[4], item[5], item[6], item[7]))
+        print("{0},{1},{2},{3},{4},{5},{6},{7},{8}".format(item[0], item[1], item[2], item[3], item[4], item[5], item[6], item[7], item[8]))
+    return lines2
 
 
 # 转换数值格式的处理函数
@@ -50,22 +52,22 @@ def convert2num(a):
 
 def dataConvert():
     # 2）转换数值格式，把销量数据中的引号、逗号等处理掉，并转换为数值
-    lines = line2RDD.map(lambda x: convert2num(x))
+    lines = line2RDD.map(lambda x: convert2num(x)) # 用自定义的处理函数处理所有数据
     for item in lines.collect():
-        print("{0},{1},{2},{3},{4},{5},{6},{7}".format(item[0], item[1], item[2], item[3], item[4], item[5], item[6], item[7]))
+        print("{0},{1},{2},{3},{4},{5},{6},{7},{8}".format(item[0], item[1], item[2], item[3], item[4], item[5], item[6], item[7], item[8]))
 
 
-def typeCnt():
+def typeCnt(lineRDD1):
     # 3）统计有多少类型的啤酒？通过distinct去重，count计数
-    types = lineRDD.map(lambda x: x[1]).distinct().count()
+    types = lineRDD1.map(lambda x: x[1]).distinct().count()
     print("统计共有" + str(types) + "种类型的啤酒。")
 
 
-def cntTopSalesBeer(n):
-    # 4）统计哪 5 种啤酒卖得最好？使用lambda表达式
-    products = lineRDD.map(lambda x: (x[1], int(x[4]) + int(x[5]) + int(x[6]))).reduceByKey(
-        lambda a, b: a + b).collect()
-    popular_list = sorted(products, key=lambda x: x[1], reverse=True)
+def cntTopSalesBeer(lineRDD1, n):
+    # 4）统计哪 5 种啤酒卖得最好？使用lambda表达式，对4、5、6三列进行求和即得到销量总和，
+    products = lineRDD1.map(lambda x: (x[1], int(x[4]) + int(x[5]) + int(x[6]))).reduceByKey(
+        lambda a, b: a + b).collect()  # 通过reduceByKey进行聚合
+    popular_list = sorted(products, key=lambda x: x[1], reverse=True)  # 排序
     i = 0
     print("卖得最好的5种啤酒如下:")
     for item in popular_list:
@@ -74,27 +76,27 @@ def cntTopSalesBeer(n):
             i += 1
 
 
-def cntGrowRate():
+def cntGrowRate(lineRDD1):
     # 5）统计哪个销售区域销售的啤酒同比去年增长最快？增长量=4+5+6+7-8列，应包含正负值。再求增长率。
     # 计算的时候注意：去年同期是月销量，不计算不准的预计销量，这里进行折算，乘以0.75。同时销量小于500的也不统计
-    lines = lineRDD.filter(lambda x: int(x[8]) >= 500)
+    lines = lineRDD1.filter(lambda x: int(x[8]) >= 500)
     products = lines.map(lambda x: (x[2], (int(x[4]) + int(x[5]) + int(x[6]) - 0.75*int(x[8])) / int(x[8]))).reduceByKey(
-        lambda a, b: a + b).collect()
+        lambda a, b: a + b).collect()  # 4+5+6-8列的0.75是为了折算去年同期的销量
     popular_list = sorted(products, key=lambda x: x[1], reverse=True)
     print("{0}区域的销量比去年同期增长最快，其增长率为{1}".format(popular_list[0][0], popular_list[0][1]))
 
 
-def cntSaleAmount():
+def cntSaleAmount(lineRDD1):
     # 6）统计每种啤酒的 11 月份前3周销量，即将列表的4 5 6列之和作为今年11月的销量
-    products = lineRDD.map(lambda x: (x[1], int(x[4]) + int(x[5]) + int(x[6]))).reduceByKey(
+    products = lineRDD1.map(lambda x: (x[1], int(x[4]) + int(x[5]) + int(x[6]))).reduceByKey(
         lambda a, b: a + b).collect()
     for item in products:
         print("{0}啤酒在今年11月的销量为{1}".format(item[0], item[1]))
 
 
-def cntTop5SaleAmount():
+def cntTop5SaleAmount(lineRDD1):
     # 7）统计啤酒卖得最好的前三个区域的 11 月份销量，组织形式为（地区名称，今年11月销量）
-    products = lineRDD.map(lambda x: (x[2], int(x[4]) + int(x[5]) + int(x[6]))).reduceByKey(
+    products = lineRDD1.map(lambda x: (x[2], int(x[4]) + int(x[5]) + int(x[6]))).reduceByKey(
         lambda a, b: a + b).collect()
     popular_list = sorted(products, key=lambda x: x[1], reverse=True)
     i = 0
@@ -106,7 +108,7 @@ def cntTop5SaleAmount():
 
 
 # 下面开始执行程序
-xlsx2txt("BeerSales.xlsx", "BeerSales.txt")
+xlsx2txt("BeerSales.xlsx", "BeerSales.txt")  # 读入原xlsx文件，并将excel文件转换为txt文件
 
 conf = SparkConf().setMaster("local").setAppName("BeerCnt")
 sc = SparkContext(conf = conf)  # 创建spark对象
@@ -116,19 +118,19 @@ lineRDD = line.map(lambda x: x.split("\t"))  # 基础按行分割，将文本转
 line2 = sc.textFile("BeerSales2.txt")  # 读入老师给出的txt文件，以供第二题使用
 line2RDD = line2.map(lambda x: x.split("\t"))
 
-print("1、去除整月销量为 0 的数据")
-remove0sales()
-print("2、转换数值格式，将符号转换为数值")
+print("\n1、去除整月销量为 0 的数据")
+lineRDD1 = remove0sales()  # 数据清洗存储
+print("\n2、转换数值格式，将符号转换为数值")
 dataConvert()
-print("3、统计有多少类型的啤酒")
-typeCnt()
-print("4、统计哪 5 种啤酒卖得最好")
-cntTopSalesBeer(5)
-print("5、统计哪个销售区域销售的啤酒同比去年增长最快")
-cntGrowRate()
-print("6、统计每种啤酒的 11 月份销量")
-cntSaleAmount()
-print("7、统计啤酒卖得最好的前三个区域的 11 月份销量")
-cntTop5SaleAmount()
+print("\n3、统计有多少类型的啤酒")
+typeCnt(lineRDD1)
+print("\n4、统计哪 5 种啤酒卖得最好")
+cntTopSalesBeer(lineRDD1, 5)
+print("\n5、统计哪个销售区域销售的啤酒同比去年增长最快")
+cntGrowRate(lineRDD1)
+print("\n6、统计每种啤酒的 11 月份销量")
+cntSaleAmount(lineRDD1)
+print("\n7、统计啤酒卖得最好的前三个区域的 11 月份销量")
+cntTop5SaleAmount(lineRDD1)
 
 sc.stop()
