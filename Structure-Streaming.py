aeb6ec26a2efe178a1985ec487c54d8542d0e004
ParatrooperAndy <1468492471@qq.com>
2022-05-27 14:31:32 +0800

    update prop

---
+++ Structure-Streaming.py
@@ -0,0 +1,40 @@
+#coding:utf-8
+import findspark
+findspark.init()
+
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import from_json
+from pyspark.sql.types import StructType, StringType
+
+if __name__ == '__main__':
+    # 创建一个SparkSession对象
+    spark = SparkSession \
+        .builder \
+        .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.11:2.3.2") \
+        .appName("pyspark_structured_streaming_kafka") \
+        .getOrCreate() 
+
+    spark.sparkContext.setLogLevel('WARN')
+    # 创建输入数据源，定义配套dataFrame
+    # 创建一个输入数据源，从kafka的test主题中接收消息。
+    df = spark.readStream \
+            .format("kafka") \
+            .option("kafka.bootstrap.servers", "localhost:9092") \
+            .option("subscribe", "test") \
+            .load()
+    words = df.selectExpr("CAST(value AS STRING)")
+    schema = StructType() \
+            .add("name", StringType()) \
+            .add("age", StringType()) \
+            .add("sex", StringType())
+    # 定义流计算过程
+    res = words.select(from_json("value", schema).alias("data")).select("data.*")
+    # 启动流计算并输出结果
+    # 默认是微批处理模式，可以设置连续处理模式，需要和检查点配合。
+    query = res.writeStream \
+            .format("console") \
+            .outputMode("append") \
+            .start()
+
+    query.awaitTermination()
+
--- spark_kafka_producer.py
+++ spark_kafka_producer.py
@@ -14,23 +14,28 @@ import random
 import time
 from kafka import KafkaProducer
 
-# 先行启动zkserver和kafka
+# 先行启动zkserver和kafka，保持服务窗口开启
+'''
+在这个实例中,使用生产者程序每0.1秒生成一个包含2个字母的单词,并写入Kafka的名称为“wordcount-topic”的主题(Topic)内。
+Spark的消费者程序通过订阅wordcount-topic,会源源不断收到单词,并且每隔8秒钟对收到的单词进行一次词频统计,
+把统计结果输出到Kafka的主题wordcount-result-topic内,同时,通过2个监控程序检查Spark处理的输入和输出结果。
+'''
 
 if __name__ == "__main__":
     producer = KafkaProducer(bootstrap_servers=['localhost:9092'])
-    '''
+    # 编写生产者程序
     while True:
         s2 = (random.choice(string.ascii_lowercase) for _ in range(2))
         word = ''.join(s2)
         future = producer.send('test', key=word, value='{"name":"caocao1","age":"32","sex":"male"}', partition=0)
         time.sleep(0.1)
-    '''
-    msg_dict = {
-        "operatorId": "test",
-        "terminalId": "123",
-        "terminalCode": "123",
-        "terminalNo": "1",  # 这里传入四个参数
-    }
-    msg = json.dumps(msg_dict).encode()
-    producer.send('tqs-admin-event-1', msg)
-    producer.close()
+    
+    # msg_dict = {
+    #     "operatorId": "test",
+    #     "terminalId": "123",
+    #     "terminalCode": "123",
+    #     "terminalNo": "1",  # 这里传入四个参数
+    # }
+    # msg = json.dumps(msg_dict).encode()
+    # producer.send('tqs-admin-event-1', msg)
+    # producer.close()
